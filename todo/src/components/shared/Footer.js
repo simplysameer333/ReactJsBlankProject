2d547d5ded8044317a44bd34d5abe3de507e0aee
simplysameer333 <samorsameer@gmail.com>
2019-09-27 15:58:37 +0100

    Create Footer.js

---
+++ todo/src/components/shared/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <div className="Footer-header">        
+         <div>
+          This is my footer
+          </div>
+        </div>
+    );
+  }
+}
+
+
+export default Footer;
