77a46249441c8b89eed77b2372c7c1bd8b7399ca
simplysameer333 <samorsameer@gmail.com>
2019-09-27 15:58:47 +0100

    Create Header.js

---
+++ todo/src/components/shared/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Headline from './Headline';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <>
+          <Headline/>         
+       </>
+    );
+  }
+}
+
+
+export default Header;
