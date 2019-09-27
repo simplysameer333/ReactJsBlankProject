e09c810936cb363bbf9b3710b4a220b76d129211
simplysameer333 <samorsameer@gmail.com>
2019-09-27 15:56:44 +0100

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import Header from './components/shared/Header'
+import Footer from './components/shared/Footer'
+import CustomNavbar from './components/shared/CustomNavbar'
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">          
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit file and save to reload.
-        </p>
+        <Header/>
+        <CustomNavbar/>        
+        <Footer/>
       </div>
     );
   }
