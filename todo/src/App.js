28110983038e2dcc9ceacde595ad9bf81fb08e57
simplysameer333 <samorsameer@gmail.com>
2019-10-04 17:11:58 +0100

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import history from './history'
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
 import CustomNavbar from './components/shared/CustomNavbar';
 import { NavigationBar } from './components/shared/NavigationBar';
 import { MainContainer } from './components/MainContainer';
-import {Layout} from './components/shared/Layout';
+import {Layout} from './components/Layout';
 import SplitPane from 'react-split-pane';
 import { Menu } from './components/shared/Menu'
 import './App.css';
@@ -22,7 +23,7 @@ class App extends Component {
     return (
       <React.Fragment>            
           <Header/>          
-          <Router>  
+          <Router history={history}>
           <NavigationBar/>  
          
           <SplitPane
