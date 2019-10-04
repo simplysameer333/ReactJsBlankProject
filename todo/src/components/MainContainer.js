6bf8a1483cf5c0402161f722eb056227c85af4c6
simplysameer333 <samorsameer@gmail.com>
2019-10-04 17:14:14 +0100

    Update MainContainer.js

--- todo/src/components/MainContainer.js
+++ todo/src/components/MainContainer.js
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { About } from './About'
 import { NoMatch } from './NoMatch'
 import { Home } from './Home'
+import { EmployeesInfo } from './EmployeesInfo'
+import { InputEmployee } from './InputEmployee'
 
 export const MainContainer = () => (
-    <Switch>
+    <Switch>        
         <Route exact path = "/" component = {Home} />
-        <Route exact path = "/about" component = {About} />
+        <Route exact path = "/inputEmployee" component = {InputEmployee} />
+        <Route exact path = "/employeesInfo" component = {EmployeesInfo} />
+        <Route exact path = "/about" component = {About} />        
         <Route component = {NoMatch} />
     </Switch>
 )
