30e8056ff5d08905ca5e79a290431b2775523c1d
simplysameer333 <samorsameer@gmail.com>
2019-10-04 17:17:01 +0100

    Update Menu.js

--- todo/src/components/shared/Menu.js
+++ todo/src/components/shared/Menu.js
@@ -5,7 +5,9 @@ import './Menu.css';
 export const Menu = () => (    
   <menu className="menu-custom">
     <Link to="/"><div className="menu-item"><span className="menu-span">Home</span></div></Link>
+    <Link to="/inputEmployee"><div className="menu-item"><span className="menu-span"></span>Add Employees</div></Link>    
+    <Link to="/employeesInfo"><div className="menu-item"><span className="menu-span"></span>Employees Info</div></Link>    
     <Link to="/topics"><div className="menu-item"><span className="menu-span"></span>Topics</div></Link>
-    <Link to="/about"><div className="menu-item"><span className="menu-span"></span>About</div></Link>
+    <Link to="/about"><div className="menu-item"><span className="menu-span"></span>About</div></Link>    
   </menu>
 )
