5490dd3c8e9d30ee8f97e07503e03fee7db87efa
simplysameer333 <samorsameer@gmail.com>
2019-10-04 17:16:43 +0100

    Delete Layout.js

--- todo/src/components/shared/Layout.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { Container} from 'react-bootstrap'
-
-export const Layout = (props) => (
-    <Container>
-        {props.children}
-    </Container>
-)
